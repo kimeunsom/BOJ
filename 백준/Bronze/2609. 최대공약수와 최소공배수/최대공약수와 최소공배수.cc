edb7dc12e47d6a861e8ec835b22b4f84fd965b07
somsom <72211340+kimeunsom@users.noreply.github.com>
2024-03-04 14:49:15 +0900

    Use 유클리드 호제법 in 최대공약수와 최소공배수.cc
    유클리드 호제법을 반복문과 재귀함수로 구현해 lcm, gcd를 구함.

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,26 +1,47 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(){
-    int A, B;
-    int min, max;
+using namespace std;
 
-    scanf("%d %d", &A, &B);
 
+// 유클리드 호제법 ㅡ 재귀 구현
+/*
+int getGcd(int a, int b) {
     
-    
-    for(int i=1; i<=A || i<B; i++){
-
-        if(A%i==0 && B%i==0){
-            max=i;
-        }
+    while(b) { // b가 0이 아닌동안 !
+        return getGcd (b, a%b);
     }
+    
+    return a;
+}*/
 
-    int a = A/max;
-    int b = B/max;
-    min = max*a*b;
 
-    printf("%d\n", max);
-    printf("%d", min);
+// 유클리드 호제법 ㅡ 반복문 구현
+int getGcd(int a, int b) {
+    
+    int temp;
+    while(b) {
+        temp = a%b; // a, b의 원본값이 변질되지 않도록 유의하며 temp 사용 !
+        a = b;
+        b = temp;
+    }
+    
+    return a;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    
+    // 항상 a가 더 크게 !
+    if(a<b) {
+        swap(a,b);
+    }
+    
+    int gcd = getGcd(a, b);
+    int lcm = a * b / getGcd(a, b); // 최대공약수 * 최소공배수 = A * B
+    
+    cout << gcd << "\n" << lcm;
 
     return 0;
 }
