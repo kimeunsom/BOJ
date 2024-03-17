c263c144f4e102352ec00a5744a4b4778622017d
somsom <72211340+kimeunsom@users.noreply.github.com>
2024-03-18 00:42:22 +0900

    [Silver III] Title: 킹, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.cc
+++ 백준/Silver/1063. 킹/킹.cc
@@ -3,97 +3,70 @@
 
 using namespace std;
 
-int BF(int n, string& k, string& d, vector<string>& moveD) {
-    
-    //주소값으로 전달해 함수에서 k,d의 값을 변형시킴
-    
-    string lastK, lastD; //움직이기 전 k,d의 위치
-    
-    for(int i=0; i<n; i++) {
-        
-        lastK=k;
-        lastD=d;
-        
-        //킹의 이동
-        if(!moveD[i].compare("T")) { //직진
-            k[1]++;
-        } else if (!moveD[i].compare("R")) {//오른쪽
-            k[0]++;
-        } else if (!moveD[i].compare("B")) {
-            k[1]--;
-        } else if (!moveD[i].compare("L")) {
-            k[0]--;
-        } else if (!moveD[i].compare("RT")) {
-            k[0]++;
-            k[1]++;
-        } else if (!moveD[i].compare("RB")) {
-            k[0]++;
-            k[1]--;
-        } else if (!moveD[i].compare("LB")) {
-            k[0]--;
-            k[1]--;
-        } else if (!moveD[i].compare("LT")) {
-            k[1]++;
-            k[0]--;
-        }
-        
-        
-        //킹이 밖으로 나갔을 때
-        if(k[0]<65 || k[0]>72 || k[1]<49 || k[1]>56) {
-            k=lastK; //방금 한 이동은 취소하고, 다음 반복 진행!
+bool isOutOfBound(char x, char y) {
+    return (x < 'A' || x > 'H' || y < '1' || y > '8');
+}
+
+int BF(int n, string& k, string& d, const vector<string>& moveD) {
+    string lastK = k, lastD = d;
+
+    for (int i = 0; i < n; i++) {
+        lastK = k;
+        lastD = d;
+
+        // 킹의 이동
+        if (moveD[i] == "T") k[1]++;
+        else if (moveD[i] == "R") k[0]++;
+        else if (moveD[i] == "B") k[1]--;
+        else if (moveD[i] == "L") k[0]--;
+        else if (moveD[i] == "RT") { k[0]++; k[1]++; }
+        else if (moveD[i] == "RB") { k[0]++; k[1]--; }
+        else if (moveD[i] == "LB") { k[0]--; k[1]--; }
+        else if (moveD[i] == "LT") { k[0]--; k[1]++; }
+
+        // 킹이 범위를 벗어난 경우
+        if (isOutOfBound(k[0], k[1])) {
+            k = lastK;
             continue;
         }
-        
-        
-        //킹이 돌이 있는 곳으로 움직일 때
-        if(!k.compare(d)) {
-            
-            //k가 움직인 만큼 d도 움직여!
-            d[0]+=(k[0]-lastK[0]);
-            d[1]+=(k[1]-lastK[1]);
-            
-            //움직인 돌이 밖으로 나갔을 때
-            if(d[0]<65 || d[0]>72 || d[1]<49 || d[1]>56) {
-                
-                d=lastD; //방금 한 이동은 취소하고, 다음 반복 진행!
-                k=lastK;
+
+        // 킹이 돌의 위치로 이동한 경우
+        if (k == d) {
+            // 돌도 같은 방향으로 이동
+            d[0] += (k[0] - lastK[0]);
+            d[1] += (k[1] - lastK[1]);
+
+            // 돌이 범위를 벗어난 경우
+            if (isOutOfBound(d[0], d[1])) {
+                d = lastD;
+                k = lastK;
                 continue;
             }
         }
-        
-        
     }
-    
+
     return 0;
 }
 
-
-
-int main()
-{
-    
-    int n; //킹이 움직인 횟수
-    string k, d; //킹과 돌의 위치
-    vector <string> moveD (n, "?"); //킹의 이동 방향 저장
+int main() {
+    int n;
+    string k, d;
+    vector<string> moveD;
     string temp;
-    
-    
-    //입력
 
+    // 입력
     cin >> k >> d >> n;
-    
-    for(int i=0; i<n; i++) {
+
+    for (int i = 0; i < n; i++) {
         cin >> temp;
         moveD.push_back(temp);
     }
-    
-    //연산
+
+    // 연산
     BF(n, k, d, moveD);
-    
-    
-    //출력
+
+    // 출력
     cout << k << '\n' << d;
 
-    
     return 0;
 }
