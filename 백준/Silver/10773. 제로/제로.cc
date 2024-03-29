1261cffe2a2a9546cbf01d9f7dfc2909b1bc565a
somsom <72211340+kimeunsom@users.noreply.github.com>
2024-03-29 15:48:27 +0900

    [Silver IV] Title: 제로, Time: 32 ms, Memory: 2816 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int getSum(stack<int> s) {
+    int sum = 0;
+    
+    while(!s.empty()) {
+        sum += s.top();
+        s.pop();
+    }
+    
+    return sum;
+}
+
+
+int main()
+{
+    int k, temp;
+    stack<int> s;
+    
+    
+    cin >> k;
+    while (k--) {
+        cin >> temp;
+        if(temp) {
+            s.push(temp); //0이 아닌 경우 push
+        } else {
+            s.pop(); //0인 경우 pop
+        }
+    }
+    
+    cout << getSum(s);
+
+    return 0;
+}
