871664c5c2dbc9dff2191bae2defa64c0de29a13
somsom <72211340+kimeunsom@users.noreply.github.com>
2024-03-31 23:18:38 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+    
+
+    int a,b; 
+
+    cin >> a >> b ;
+
+    
+
+    long long sum = a+b; // 합
+
+    long long sub = a-b; // 차
+
+    
+
+    cout << sum*sub;
+
+    
+
+    return 0;
+
+}
