c0ee7c24c6d3ecca6a1e0e04a4491b60d503a293
somsom <72211340+kimeunsom@users.noreply.github.com>
2024-04-05 21:17:58 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int t, a, b;
+    cin >> t;
+    
+    while(t--) {
+        cin >> a >> b;
+        cout << a+b << "\n";
+    }
+    
+    
+
+    return 0;
+}
