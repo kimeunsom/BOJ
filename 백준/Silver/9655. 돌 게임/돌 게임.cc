4b8be8a541e1c14d72a4b7fb169d7edd78653431
somsom <72211340+kimeunsom@users.noreply.github.com>
2024-04-06 23:38:34 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=0;
+    scanf("%d", &n);
+    
+    if(n%2==0) {
+        printf("CY");
+    } else {
+        printf("SK");
+    }
+
+    return 0;
+}
