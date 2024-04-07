cb3e2b66898ad550ae2982b256b93ed586a71b1d
somsom <72211340+kimeunsom@users.noreply.github.com>
2024-04-07 20:37:05 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    string str;
+    int x;
+    queue<int> q;
+    
+    while(n--) {
+        
+        cin >> str;
+        
+        if (str=="push") { 
+            
+            cin >> x;
+            q.push(x);
+            
+        } else if(str=="pop") {
+            
+            if(q.empty()) {
+                cout << "-1\n";
+            } else {
+                cout << q.front() << "\n";
+                q.pop();
+            }
+            
+        } else if(str=="size") {
+            
+            cout << q.size() << "\n";
+
+            
+        } else if(str=="empty") {
+            
+            cout << q.empty() << "\n";
+            
+        } else if(str=="front") {
+            
+            if(q.empty()) {
+                cout << "-1\n";
+            } else {
+                cout << q.front() << "\n";
+            }
+            
+        } else if(str=="back") {
+            
+            if(q.empty()) {
+                cout << "-1\n";
+            } else {
+                cout << q.back() << "\n";
+            }
+            
+            
+        }
+    }
+    
+    return 0;
+}
