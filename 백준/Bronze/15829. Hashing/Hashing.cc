27ed2e8dda7ea01d79e9113ac19646cdad45d8c4
cotton <72211340+kimeunsom@users.noreply.github.com>
2024-07-21 02:58:39 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -3,13 +3,16 @@
 
 using namespace std;
 
+const int m = 1234567891;
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
     int l;
-    long hash = 0;
+    long long hash = 0;
+    long long r = 1;
     string str; 
     
     // 입력
@@ -17,9 +20,11 @@ int main()
     
     // 연산
     for(int i=0; i<l; i++) {
-        hash += (str[i]-'a'+1) * pow(31, i);
+        hash += ((str[i]-'a'+1) * r) % m;
+        
+        r = (r * 31) % m;
     }
-    hash %= 1234567891;
+    hash %= m;
     
     // 출력
     cout << hash;
