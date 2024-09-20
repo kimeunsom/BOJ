220929e44d601f18e985179d389827df6507c6bd
cotton <72211340+kimeunsom@users.noreply.github.com>
2024-09-20 11:13:22 +0900

    [Silver I] Title: 회의실 배정, Time: 76 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef pair<int, int> pi;
+
+int main()
+{
+    int n, cnt=0;
+    vector<pi> arr;
+    
+    cin >> n;
+    arr.assign(n, {0,0});
+    
+    for(int i=0; i<n; i++) {
+        cin >> arr[i].second >> arr[i].first;
+    }
+    
+    sort(arr.begin(), arr.end());
+    
+    int finish_time = 0;
+    for(int i=0; i<n; i++) {
+        if (finish_time > arr[i].second) {
+            continue;
+        }
+        cnt ++;
+        finish_time = arr[i].first;
+    }
+
+
+    cout << cnt;
+    
+    return 0;
+}
