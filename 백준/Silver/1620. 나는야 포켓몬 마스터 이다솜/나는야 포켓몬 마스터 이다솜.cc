30241d6c652b6578a97986767e7329b3c7102d62
cotton <72211340+kimeunsom@users.noreply.github.com>
2024-09-27 22:01:13 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 104 ms, Memory: 25140 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,37 +1,34 @@
 #include <iostream>
-#include <map>
+#include <unordered_map> // unordered_map 사용
 
 using namespace std;
-
-
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
-    // map <키, 밸류> 이름
     int n, m;
     string input;
-    map <int, string> name; // 번호로 이름찾기
-    map <string, int> num; // 이름으로 번호찾기
+    unordered_map<int, string> name; // 번호로 이름 찾기
+    unordered_map<string, int> num;  // 이름으로 번호 찾기
 
     // 입력
     cin >> n >> m;
-    for(int i=1; i<=n; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >> input;
         num[input] = i;
         name[i] = input;
     }
     
     // 출력 및 연산
-    for(int i=0; i<m; i++) {
+    for (int i = 0; i < m; i++) {
         cin >> input;
-        
-        if(input[0]>='A' && input[0]<='Z') { // 입력이 문자
-            cout << num[input]  << '\n';
-        } else { // 입력이 숫자
+
+        if (isdigit(input[0])) { // 입력이 숫자일 경우
             cout << name[stoi(input)] << '\n';
+        } else { // 입력이 문자일 경우
+            cout << num[input] << '\n';
         }
     }
 
