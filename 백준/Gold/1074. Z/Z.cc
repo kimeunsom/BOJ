d5e5b24078b45d095bd32c38a9a6ad924e60e482
cotton <72211340+kimeunsom@users.noreply.github.com>
2025-03-21 02:32:45 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -4,17 +4,12 @@
 using namespace std;
 
 int func(int n, int r, int c) {
-    int x=0;
     if(n==0) return 0;
     
-    // 몇 번째 사분면인지 체크 ; 4분면의 기준이 2의 n-1승.
-    int power = static_cast<int>(pow(2, n-1));
-    
-    if(r<power && c>=power) x=1;
-    else if(r>=power && c<power) x=2;
-    else if(r>=power && c>=power) x=3;
-    
-    return x*power*power + func(n-1, r%power, c%power);
+    int power = 1 << (n - 1);    
+    int quadrant = (r >= power) * 2 + (c >= power);
+
+    return quadrant*power*power + func(n-1, r%power, c%power);
 }
 
 int main()
@@ -24,7 +19,6 @@ int main()
     
     int n, r, c;
     cin >> n >> r >> c;
-
     cout << func(n, r, c);
 
     return 0;
