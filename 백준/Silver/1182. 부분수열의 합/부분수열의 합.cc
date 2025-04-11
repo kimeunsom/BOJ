c30130ff7688f5c455075bbbe50b00e929fc7c2f
cotton <72211340+kimeunsom@users.noreply.github.com>
2025-04-11 19:27:34 +0900

    [Silver II] Title: 부분수열의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -3,24 +3,21 @@
 #include <algorithm>
 
 using namespace std;
-int n, s, cnt, idx, sum;
+int n, s, cnt;
 bool flag;
 int arr[30];
 bool isUsed[30];
 
-void func(int idx) {
-    if(idx == n) { // 합이 s와 같을 때 종료.
-        if(sum==s) cnt++;
+void func(int x, int sum) {
+    if(x == n) { // 배열 다 돌았거나,
+        if(sum==s) cnt++; // 합이 s와 같을 때 종료.
         return;
     }
-     
     // 해당 수를 더한 경우.
-    sum+=arr[idx];
-    func(idx+1);
+    func(x+1, sum+arr[x]);
     
     // 해당 수를 더하지 않은 경우.
-    sum-=arr[idx];
-    func(idx+1);
+    func(x+1, sum);
 }
 
 int main()
@@ -33,7 +30,7 @@ int main()
         cin >> arr[i];
     }
     
-    func(0);
+    func(0, 0);
     if(s==0) cnt--; // 아무것도 선택되지 않은 경우 제외.
     
     cout << cnt;
